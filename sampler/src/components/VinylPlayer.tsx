7793d21dc0304ecb353a16c5799b6b869cb8224d
Noé Plantier <plantiernoe50@gmail.com>
2025-01-30 19:41:11 +0100

    Vinyl Vintage Style integration

--- sampler/src/components/VinylPlayer.tsx
+++ sampler/src/components/VinylPlayer.tsx
@@ -18,8 +18,11 @@ const VinylPlayer = ({ sample }) => {
   }, [isPlaying]);
 
   return (
-    <div className="vinyl-player">
-      <div className="vinyl-disc" style={{ transform: `rotate(${rotation}deg)` }}>
+    <div className="vinyl-player" style={{paddingBottom:"10rem", paddingTop:"2rem"}}>
+      <h1 className="main-title" style={{fontSize:"10rem", color:"beige", textAlign:"center", marginBottom:"-1rem"}}>SAMPLER</h1>
+      <h1 className="main-title" style={{fontSize:"5rem", color:"beige", textAlign:"center"}}>Free rights musics for your samples.</h1>
+
+      <div className="vinyl-disc" style={{transform: `rotate(${rotation}deg)` }}>
         <div className="vinyl-label">
           <span>{sample.name}</span>
         </div>
@@ -29,13 +32,13 @@ const VinylPlayer = ({ sample }) => {
           className="vinyl-btn"
           onClick={() => setIsPlaying(!isPlaying)}
         >
-          {isPlaying ? <Pause className="h-6 w-6" /> : <Play className="h-6 w-6" />}
+          {isPlaying ? <Pause className="h-6 w-6" /> : <Play className="h-8 w-8" />}
         </button>
         <button className="vinyl-btn">
-          <RotateCcw className="h-6 w-6" />
+          <RotateCcw className="h-8 w-8" />
         </button>
         <div className="vinyl-volume">
-          <Volume2 className="h-4 w-4" />
+          <Volume2 className="h-8 w-8" />
           <input type="range" min="0" max="100" className="vintage-slider" />
         </div>
       </div>
